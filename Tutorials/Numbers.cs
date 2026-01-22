822aaf39b8fd9b878e04a543953a0d403438238a
Ragnqrok <scrantonsamuel@gmail.com>
2026-01-22 10:54:42 -0500

    Update Numbers.cs

--- Tutorials/Numbers.cs
+++ Tutorials/Numbers.cs
@@ -1,95 +1,18 @@
-int a =18;
-int b = 6;
-int c = a + b;
-Console.WriteLine(c);
+//WorkingWithInts();
+//OrderPrecedence();
+//Limits();
+//doubles();
+//decimals();
 
-
-//
-c = a - b;
-Console.WriteLine(c);
-
-
-//
-c = a * b;
-Console.WriteLine(c);
-
-
-//
-c = a / b;
-Console.WriteLine(c);
-
-
-
-
-//WorkWithIntegers();
-
-
-
-void WorkWithIntegers()
-{
-    int a =18;
-    int b = 6;
-    int c = a + b;
-    Console.WriteLine(c);
-
-
-
-    //
-    c = a - b;
-    Console.WriteLine(c);
-
-
-    //
-    c = a * b;
-    Console.WriteLine(c);
-
-
-
-    //
-    c = a / b;
-    Console.WriteLine(c);
-}
-
-
-
-
-int a = 5;
-int b = 4;
-int c = 2;
-int d = a + b * c;
-Console.WriteLine(d);
-
-d = (a + b) * c;
-Console.WriteLine(d);
-
-
-d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-Console.WriteLine(d);
-
-
-
-
-int e = 7;
-int f = 4;
-int g = 3;
-int h = (e + f) / g;
-Console.WriteLine(h);
-
-
-
-// WorkWithIntegers();
-OrderPrecedence();
-
-void WorkWithIntegers()
+void WorkingWithInts()
 {
     int a = 18;
     int b = 6;
     int c = a + b;
     Console.WriteLine(c);
 
-
-    // subtraction
-    c = a - b;
+    //Subtraction
+    c = a -b;
     Console.WriteLine(c);
 
     // multiplication
@@ -97,6 +20,7 @@ void WorkWithIntegers()
     Console.WriteLine(c);
 
     // division
+
     c = a / b;
     Console.WriteLine(c);
 }
@@ -109,11 +33,13 @@ void OrderPrecedence()
     int d = a + b * c;
     Console.WriteLine(d);
 
+
     d = (a + b) * c;
     Console.WriteLine(d);
 
-    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    d = (a + b) - 6 * c + (12 * 4) / 3 * 12;
     Console.WriteLine(d);
+
 
     int e = 7;
     int f = 4;
@@ -122,62 +48,50 @@ void OrderPrecedence()
     Console.WriteLine(h);
 }
 
+void Limits()
+{
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b) % c;
+    Console.WriteLine($"Quotient: {d}");
+    Console.WriteLine($"Remainder: {e}");
 
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    int what = max + 3;
+    Console.WriteLine($"An example of overflow: {what}");
+    Console.WriteLine($"The range of integers is {min} to {max}");
+}
 
+void doubles()
+{
+    double a = 19;
+    double b = 23;
+    double c = 8; 
+    double d = ( a + b) / c;
+    Console.WriteLine(d);
 
-int a = 7;
-int b = 4;
-int c = 3;
-int d = (a + b) / c;
-int e = (a + b) % c;
-Console.WriteLine($"quotient: {d}");
-Console.WriteLine($"remainder: {e}");
+    double max = double.MaxValue;
+    double min = double.MinValue;
+    Console.WriteLine($"The range pf double is {min} to {max}");
 
+    double third = 1.0 / 3.0;
+    Console.WriteLine(third);
+}
 
+void decimals()
+{
+    Decimal min = Decimal.MinValue;
+    Decimal max = Decimal.MaxValue;
+    Console.WriteLine($"The range for decimals is {min} to {max}");
 
-int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a/b);
 
-
-int what = max + 3;
-Console.WriteLine($"An example of overflow: {what}");
-
-
-double a = 5;
-double b = 4;
-double c = 2;
-double d = (a + b) / c;
-Console.WriteLine(d);
-
-
-double a = 19;
-double b = 23;
-double c = 8;
-double d = (a + b) / c;
-Console.WriteLine(d);
-
-
-double max = double.MaxValue;
-double min = double.MinValue;
-Console.WriteLine($"The range of double is {min} to {max}");
-
-
-double third = 1.0 / 3.0;
-Console.WriteLine(third);
-
-
-
-decimal min = decimal.MinValue;
-decimal max = decimal.MaxValue;
-Console.WriteLine($"The range of the decimal type is {min} to {max}");
-
-
-
-double a = 1.0;
-double b = 3.0;
-Console.WriteLine(a / b);
-
-decimal c = 1.0M;
-decimal d = 3.0M;
-Console.WriteLine(c / d);
+    Decimal c = 1.0m;
+    Decimal d = 3.0m;
+    Console.WriteLine(c/d);
+}
