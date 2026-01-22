ccfdf7c37a415e86643af86291073aa7d04c3381
Ragnqrok <scrantonsamuel@gmail.com>
2026-01-22 10:06:34 -0500

    Add files via upload

--- Tutorials/HelloWorld.cs
+++ Tutorials/HelloWorld.cs
@@ -1,1 +1,54 @@
-Console.WriteLine("Hello, World!")
+String aFriend = "Bill";
+Console.WriteLine(aFriend);
+
+aFriend = "Maira";
+Console.WriteLine(aFriend);
+
+Console.WriteLine("Hello " + aFriend);
+
+Console.WriteLine($"Hello {aFriend}");
+
+
+
+
+String firstfriend = "Maira";
+String secondfriend = "Sage";
+Console.WriteLine($"My friends are {firstfriend} and {secondfriend}");
+
+Console.WriteLine($"The name {firstfriend} has {firstfriend.Length} letters.");
+Console.WriteLine($"The name {secondfriend} has {secondfriend.Length} letters.");
+
+String greeting = "      Hello World!           ";
+
+String trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+Console.WriteLine($"{trimmedGreeting}");
+
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+
+
+String sayHello = "Hello World!";
+
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+
+String songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.Contains("goodbye"));
+Console.WriteLine(songLyrics.Contains("greetings"));
+
+
+Console.WriteLine(songLyrics.StartsWith("you"));
+Console.WriteLine(songLyrics.EndsWith("hello"));
+
+
